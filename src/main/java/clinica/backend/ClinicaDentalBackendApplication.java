dd97d644f04dd40c10ddc72b8f6101f6a5f8b3f0
JonathanRAD <jonathanrujel4@gmail.com>
2025-11-14 09:44:57 -0500

    commit inicial

---
+++ src/main/java/clinica/backend/ClinicaDentalBackendApplication.java
@@ -0,0 +1,13 @@
+package clinica.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaDentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaDentalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clinica/backend/ClinicaDentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package clinica.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaDentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
